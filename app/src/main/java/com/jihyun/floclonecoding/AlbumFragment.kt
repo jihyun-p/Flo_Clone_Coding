03672ad4ccdd23bf21d918d7fd133dada3cfac23
Jihyun <twentyt.park@gmail.com>
2024-11-14 21:50:20 +0900

    2nd week - Fragment 전환까지

---
+++ app/src/main/java/com/jihyun/floclonecoding/AlbumFragment.kt
@@ -0,0 +1,25 @@
+package com.jihyun.floclonecoding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jihyun.floclonecoding.databinding.FragmentAlbumBinding
+
+class AlbumFragment : Fragment() { // HomeFragment에서 앨범 사진 누르면 AlbumFragment로 이동
+    lateinit var binding : FragmentAlbumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater,container,false)
+
+        binding.albumBackIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
+        }
+        return binding.root
+    }
+}
