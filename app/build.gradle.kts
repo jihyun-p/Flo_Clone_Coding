6dabfe9d06a91b482ed07737b30f447a942d48b4
Jihyun <twentyt.park@gmail.com>
2024-11-19 00:41:01 +0900

    mainPlayerCl 클릭 시 SongActivity 나오지 않는 문제 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.jihyun.floclonecoding"
-    compileSdk = 34
+    compileSdk = 35
 
     viewBinding {
         enable = true
--- app/src/main/java/com/jihyun/floclonecoding/AlbumFragment.kt
+++ app/src/main/java/com/jihyun/floclonecoding/AlbumFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.jihyun.floclonecoding.databinding.FragmentAlbumBinding
 
@@ -20,6 +21,11 @@ class AlbumFragment : Fragment() { // HomeFragment에서 앨범 사진 누르면
         binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
         }
+
+        binding.songLalacLayout.setOnClickListener {
+            Toast.makeText(activity,"LILAC",Toast.LENGTH_SHORT).show()
+        }
+
         return binding.root
     }
 }
