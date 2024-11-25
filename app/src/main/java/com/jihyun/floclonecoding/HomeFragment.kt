3de5c208236c01a28f60948d36e73889936e346d
Jihyun <twentyt.park@gmail.com>
2024-11-25 22:47:29 +0900

    3주차 플로 클론 코딩 실습 완료

--- app/src/main/java/com/jihyun/floclonecoding/HomeFragment.kt
+++ app/src/main/java/com/jihyun/floclonecoding/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.jihyun.floclonecoding.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -21,6 +22,16 @@ class HomeFragment : Fragment() {
         binding.homeAlbumImgIv1.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, AlbumFragment()).commitAllowingStateLoss()
         }
+
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+
+        binding.homeBannerVp.adapter = bannerAdapter // viewpager와 adapter 연결
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 배너가 좌우로 스크롤 되게 함
+
         return binding.root
     }
 }
--- app/src/main/java/com/jihyun/floclonecoding/LockerFragment.kt
+++ app/src/main/java/com/jihyun/floclonecoding/LockerFragment.kt
@@ -1,6 +1,35 @@
 package com.jihyun.floclonecoding
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.jihyun.floclonecoding.databinding.FragmentLockerBinding
 
 class LockerFragment : Fragment() {
+
+    lateinit var binding: FragmentLockerBinding
+    private val information = arrayListOf("저장한 곡", "음악파일")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        val lockerAdapter = LockerVPAdapter(this)
+        binding.lockerContentVp.adapter = lockerAdapter
+        TapLayoutMediator(binding.lockerContentTb, binding.lockerContentVp){
+                tab, position ->
+            tab.text = information[position]
+        }.attach()
+
+        binding.lockerLoginTv.setOnClickListener {
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }
+
+        return binding.root
+    }
 }
